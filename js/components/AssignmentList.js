9af753db2ad8f99ec0f0f66b0a3302bc7231e1db
Bartosz Piechowiak <piechowiaq@gmail.com>
2022-12-21 15:15:15 +0100

    It's All so Easy

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -4,10 +4,23 @@ export default {
     components: { Assignment},
     template:`
      <section v-show="assignments.length">
-        <h2 class="font-bold mb-2">{{ title }}</h2>
+        <h2 class="font-bold mb-2">
+          {{ title }}
+          <span>({{ assignments.length }})</span>
+        </h2>
+        
+        <div class="flex gap-2">
+          <button 
+              @click="currentTag = tag"
+              v-for="tag in tags" 
+              class="border rounded text-xs px-1 py-px"
+              :class="{
+                'border-blue-500 text-blue-500': tag === currentTag
+              }">{{ tag }}</button>
+        </div>
 
-        <ul class="border border-gray-600 divide-y divide-gray-600">
-           <Assignment   v-for="assignment in assignments"
+        <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
+           <Assignment   v-for="assignment in filterAssignments"
                          :key="assignment.id"
                          :assignment="assignment"
            ></Assignment>
@@ -19,5 +32,25 @@ export default {
         assignments: Array,
         title: String,
 
+    },
+
+    data() {
+
+        return {
+            currentTag: 'all'
+        };
+    },
+
+    computed: {
+        filterAssignments(){
+            if (this.currentTag === 'all') {
+                return this.assignments;
+            }
+            return this.assignments.filter(a => a.tag === this.currentTag);
+
+        },
+        tags(){
+            return ['all', ...new Set(this.assignments.map(a => a.tag))];
+        }
     }
 }
---
+++ js/components/AssignmentTags.js
@@ -0,0 +1,6 @@
+export default {
+    template: `
+    <button v-for="tag in tags" type="submit"><button/>
+    `,
+
+}
